f3945075e1bab6d6c05c914fd3f1c052e2ca031f
takoiryu <takoiryu@gmail.com>
2026-09-11 16:32:54 +0200

    exo 2 preparation

---
+++ exercice2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func calculerMoyenne(somme int, nombre int) float64{
+
+}
+func trouverMaximum(...){
+
+}
+func trouverMinimum(...){
+
+}
+func afficherResultat(moyenne float64){
+	
+}
